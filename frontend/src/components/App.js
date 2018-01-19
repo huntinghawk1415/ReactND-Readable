2140a87ddbb2934e47669f6528b951ab47ecad2a
Jacob Lockett <jacobwyattlockett@gmail.com>
2018-01-19 19:57:01 +0000

    added state logic for Home.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,17 @@ import Home from './Home';
 import '../style/App.css';
 
 class App extends Component {
+  state = {
+    view: 'home'
+  }
   render() {
+    const {view} = this.state;
     return (
-      <div className="container-fluid">
-        <Home />
-      </div>
+      <div className='container-fluid'>{
+        view === 'home'
+          ? <Home />
+          : null
+      }</div>
     );
   }
 }
