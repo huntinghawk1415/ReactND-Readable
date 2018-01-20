93e26850954f84b7cd83dca44a4fab204af31e37
Jacob <jacobwyattlockett@gmail.com>
2018-01-19 23:42:11 -0500

    added: container to center text

--- frontend/src/components/App/Navbar.js
+++ frontend/src/components/App/Navbar.js
@@ -2,27 +2,29 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Readable</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Posts</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link" href="#">Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login/Logout</a>
-          </li>
-        </ul>
-      </div>
+    <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">Readable</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarToggleExternalContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Posts</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link" href="#">Profile</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Login/Logout</a>
+            </li>
+          </ul>
+        </div>
+        </div>
     </nav>
   )
 }
