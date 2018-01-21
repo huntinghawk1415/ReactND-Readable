e82b8d73834b2a21b88621f1f8e86527d4f05436
Jacob <jacobwyattlockett@gmail.com>
2018-01-21 17:54:50 -0500

    added: react-router

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import {Route} from 'react-router-dom';
 import Home from '../Home';
 import Posts from '../Posts';
 import Profile from '../Profile';
@@ -7,28 +8,28 @@ import Login from '../Login';
 import '../../style/App.css';
 
 class App extends Component {
-  state = {
-    view: 'login'
-  }
   render() {
-    const {view} = this.state;
     return (
       <div className='container-fluid default-settings'>
         <Navbar />
-        <div className='h-100'>{
-          view === 'home'
-            ? <Home />
-            :
-          view === 'posts'
-            ? <Posts />
-            :
-          view === 'profile'
-            ? <Profile />
-            :
-          view === 'login'
-            ? <Login />
-            : null
-        }</div>
+        <div className='h-100'>
+          <Route
+            exact path='/'
+            component={Home}
+          />
+          <Route
+            path='/posts'
+            component={Posts}
+          />
+          <Route
+            path='/profile'
+            component={Profile}
+          />
+          <Route
+            path='/login'
+            component={Login}
+          />
+        </div>
       </div>
     );
   }
--- frontend/src/components/App/Navbar.js
+++ frontend/src/components/App/Navbar.js
@@ -1,26 +1,42 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className='navbar navbar-expand-lg sticky-top navbar-dark bg-dark'>
       <div className='container'>
-        <a className='navbar-brand' href='#'>Readable</a>
+        <Link
+          className='navbar-brand'
+          to='/'
+        >Readable</Link>
         <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
         </button>
         <div className='collapse navbar-collapse' id='navbarToggleExternalContent'>
           <ul className='navbar-nav ml-auto'>
             <li className='nav-item'>
-              <a className='nav-link' href='#'>Home</a>
+              <Link
+                className='nav-link'
+                to='/'
+              >Home</Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='#'>Posts</a>
+              <Link
+                className='nav-link'
+                to='posts'
+              >Posts</Link>
             </li>
             <li className='nav-item dropdown'>
-              <a className='nav-link' href='#'>Profile</a>
+              <Link
+                className='nav-link'
+                to='profile'
+              >Profile</Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='#'>Login/Logout</a>
+              <Link
+                className='nav-link'
+                to='login'
+              >Login/Logout</Link>
             </li>
           </ul>
         </div>
