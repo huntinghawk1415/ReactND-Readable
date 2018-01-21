7455a26c70ed6f07ea4b5751a678bb4c3108fcd4
Jacob <jacobwyattlockett@gmail.com>
2018-01-21 15:38:40 -0500

    misc: muliple untracked changes; needs review

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,68 @@ import React, {Component} from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div className='container'>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
