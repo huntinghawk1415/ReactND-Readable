9ef129db4c718aa80eb9f97edc0ae118e002b332
Jacob <jacobwyattlockett@gmail.com>
2018-01-21 16:14:26 -0500

    initial commit

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -3,7 +3,137 @@ import React, {Component} from 'react';
 class Posts extends Component {
   render() {
     return (
-      <div className='display-1'>Hello there</div>
+      <div className='container h-100'>
+        <div className='row display-4 pt-5'>
+          Trending Now
+        </div>
+        <div className='row'>
+          <div className='col-8 mt-5 border border-dark'>
+            <div className='row'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-3 mt-5 ml-auto border border-dark'>
+            <div className='row'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              <div className='col border border-dark'>
+                <div className='row bg-primary'>
+                  Column 1
+                </div>
+                <div className='row bg-secondary'>
+                  Column 2
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
