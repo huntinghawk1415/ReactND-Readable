22547ab4c8d30feaf0808d3e21be20d72e2edd9d
Jacob <jacobwyattlockett@gmail.com>
2018-01-21 17:27:23 -0500

    added: login username and password

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,7 +3,28 @@ import React, {Component} from 'react';
 class Login extends Component {
   render() {
     return (
-      <div>test</div>
+      <div className='container h-100'>
+        <div className='row h-100 d-flex justify-content-center align-items-center'>
+          <div>
+            <div>
+              Username
+            </div>
+            <div>
+              <form>
+                <input placeholder='username here'/>
+              </form>
+            </div>
+            <div className='pt-3'>
+              Passord
+            </div>
+            <div>
+              <form>
+                <input placeholder='password here' type='password'/>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
