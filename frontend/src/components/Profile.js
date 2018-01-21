1d7a53ede714c7cb95e22dcf27a1d416b4ee0740
Jacob <jacobwyattlockett@gmail.com>
2018-01-21 16:45:36 -0500

    added: stats for posts, comments, votes

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,7 +3,37 @@ import React, {Component} from 'react';
 class Profile extends Component {
   render() {
     return (
-      <div>hello</div>
+      <div className='container h-100'>
+        <div className='row display-4 pt-5'>
+          Username Here
+        </div>
+        <div className='row h-25 mt-5 border border-dark'>
+          <div className='col p-4'>
+            <h2 className='row h-50 m-0 d-flex justify-content-center'>
+              Posts
+            </h2>
+            <h4 className='row h-50 m-0 font-weight-normal d-flex justify-content-center'>
+              20
+            </h4>
+          </div>
+          <div className='col p-4'>
+            <h2 className='row h-50 m-0 d-flex justify-content-center'>
+              Comments
+            </h2>
+            <h4 className='row h-50 m-0 font-weight-normal d-flex justify-content-center'>
+              20
+            </h4>
+          </div>
+          <div className='col p-4'>
+            <h2 className='row h-50 m-0 d-flex justify-content-center'>
+              Votes
+            </h2>
+            <h4 className='row h-50 m-0 font-weight-normal d-flex justify-content-center'>
+              20
+            </h4>
+          </div>
+        </div>
+      </div>
     )
   }
 }
