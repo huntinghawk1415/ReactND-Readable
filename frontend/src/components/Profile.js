7d51c7e5666946e829fdcc0eb275faf02986ed68
Jacob <jacobwyattlockett@gmail.com>
2018-01-21 17:09:51 -0500

    added: recent posts

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -7,8 +7,8 @@ class Profile extends Component {
         <div className='row display-4 pt-5'>
           Username Here
         </div>
-        <div className='row h-25 mt-5 border border-dark'>
-          <div className='col p-4'>
+        <div className='row h-25 mt-5 pb-5'>
+          <div className='col p-4 bg-dark text-light border border-white'>
             <h2 className='row h-50 m-0 d-flex justify-content-center'>
               Posts
             </h2>
@@ -16,7 +16,7 @@ class Profile extends Component {
               20
             </h4>
           </div>
-          <div className='col p-4'>
+          <div className='col p-4 bg-dark text-light border border-white'>
             <h2 className='row h-50 m-0 d-flex justify-content-center'>
               Comments
             </h2>
@@ -24,13 +24,46 @@ class Profile extends Component {
               20
             </h4>
           </div>
-          <div className='col p-4'>
+          <div className='col p-4 bg-dark text-light border border-white'>
             <h2 className='row h-50 m-0 d-flex justify-content-center'>
               Votes
             </h2>
             <h4 className='row h-50 m-0 font-weight-normal d-flex justify-content-center'>
               20
             </h4>
+          </div>
+        </div>
+        <h5 className='row mt-5'>
+          Recent Posts
+        </h5>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col border border-dark'>
+            <div className='row bg-primary'>
+              Column 1
+            </div>
+            <div className='row bg-secondary'>
+              Column 2
+            </div>
           </div>
         </div>
       </div>
